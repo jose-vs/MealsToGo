d5373376f3aaecde7d45aec68169b584e64b8ebc
Jose Santos <67150374+jose-vs@users.noreply.github.com>
2023-04-05 11:32:06 +1200

    fix folder name

--- src/services/restaurant/restaurant.service.js
+++ src/services/restaurant/restaurant.service.js
@@ -1,4 +1,4 @@
-import { mockImages, mocks } from "./mock ";
+import { mockImages, mocks } from "./mock";
 import camelize from "camelize";
 
 export const restaurantRequest = (location = "37.7749295,-122.4194155") => {
